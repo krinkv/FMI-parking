5021f1eb13a646f7d7d8b9dabb584b699ef960b5
Bozhidar Dimitrov <dbozhidar@vmware.com>
2023-01-23 19:24:03 +0200

    finetune login page

--- FMI-parking/client/components/login/javascript/login.js
+++ FMI-parking/client/components/login/javascript/login.js
@@ -1,12 +1,7 @@
 function login() {
-    const inputs = document.querySelectorAll("input"); // all input fields
+    const inputs = document.querySelectorAll("input"); // all input fields from login form
     const form = document.getElementById("login-form"); // the login form
     const responseDiv = document.getElementById("response-message"); // div which will contain response message
-    const toRegistrationBtn = document.getElementById("to-registration-btn"); // button, redirecting to the registration page
-
-    toRegistrationBtn.addEventListener('click', () => {
-        window.location.href = "../register/registration_form.html"; // redirect the user over to the registration page with an option to go back from the browser
-    });
 
     form.addEventListener('submit', (event) => {
         event.preventDefault(); // prevent the form from resetting
@@ -25,40 +20,45 @@ function login() {
 
         // send the login data to the server
         checkLoginData(data)
-        .then((responseMessage) => {
-            if (responseMessage["status"] === "ERROR") {
-                throw new Error(responseMessage["message"]);
-            }
-            else {
-                window.location.replace("../account/account.html"); // if the login resulted in success, then redirect the user over to his account page
-            }
-        })
-        .catch((errorMessage) => {
-            createErrorDivContent(responseDiv, errorMessage); // if the login resulted in an error, then display an error messages
-        })
+            .then((responseMessage) => {
+                if (responseMessage["status"] === "ERROR") {
+                    throw new Error(responseMessage["message"]);
+                }
+                else {
+                    window.location.replace("../account/account_view.html"); // if the login resulted in success, then redirect the user over to his account page
+                }
+            })
+            .catch((errorMessage) => {
+                createErrorDivContent(responseDiv, errorMessage); // if the login resulted in an error, then display an error messages
+            })
     })
 };
 
 /* sends the inputted data over to the backend to authenticate the user */
 function checkLoginData(data) {
-    // TODO login endpoint 
-    return fetch("../../backend/...", {
+    return fetch("../../backend/api/login/login_user.php", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(data)
     })
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        return data;
-    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            return data;
+        })
 }
 
 
 function createErrorDivContent(div, response) {
+    div.innerHTML=null;
+    // create the image and append it's attributes src and alt
+    let errorImage = document.createElement("img");
+    errorImage.src = "./images/error_response.png";
+    errorImage.alt = "white exclamation mark on red background"
+
     // create the text of the response
     let messageContainer = document.createElement("span");
     let message = document.createElement("p");
@@ -66,8 +66,33 @@ function createErrorDivContent(div, response) {
     messageContainer.append(message);
 
     // append the img and span to the div
+    div.appendChild(errorImage);
     div.appendChild(messageContainer);
 
     // show it to the user
     div.classList.remove("no-show");
+}
+
+function showDiv(div, message) {
+    div.innerHTML = null;
+    // create the image of the error (a white exclamation mark)
+    let statusImage = document.createElement("img");
+
+    // attach image attributes src and alt
+    statusImage.src = "./images/error_response.png";
+    statusImage.alt = "white exclamation mark on red background";
+
+    // toggle classes
+    div.classList.add("error");
+    div.classList.remove("no-show");
+
+    // create error text and append to span element
+    let messageContainer = document.createElement("span");
+    let responseMessage = document.createElement("p");
+    responseMessage.textContent = message;
+    messageContainer.appendChild(responseMessage);
+
+    // append all created elements to the response div
+    div.appendChild(statusImage);
+    div.appendChild(messageContainer);
 }
