84f872f1ef5c59d3eae1154f2243ec6704d5cc31
BoyanVD <46243260+BoyanVD@users.noreply.github.com>
2023-01-29 19:15:04 +0200

    POST handling modified

--- FMI-parking/server/controller/user_login.php
+++ FMI-parking/server/controller/user_login.php
@@ -1,22 +1,19 @@
 <?php 
 require_once("../repository/database_queries.php");
 
-$input_data = file_get_contents("php://input");
-$user_data = json_decode($input_data, true); // validation
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-$email = $user_data["email"];
-$password = $user_data["password"];
+    $user_db = getUserByEmail($email); // validation
 
-$user_db = getUserByEmail($email); // validation
+    // Check if password is correct
 
-// Check if password is correct
+    session_start();
+    $_SESSION["user"] = $db_user;
 
-session_start();
-$_SESSION["user"] = $db_user;
-
-setcookie("email", $user["email"], time() + 1000, "/");
-setcookie("password", $user["password"], time() + 1000, "/");
-
-http_response_code(200);
-exit(json_encode(["status" => "SUCCESS", "message" => "Successfull login"]));
+    http_response_code(200);
+    exit(json_encode(["status" => "SUCCESS", "message" => "Successfull login"]));
+  }
 ?>
--- FMI-parking/server/controller/user_registration.php
+++ FMI-parking/server/controller/user_registration.php
@@ -2,28 +2,49 @@
 require_once("../repository/database_queries.php");
 require_once("../model/user.php");
 
-$input_data = file_get_contents("php://input");
-$user_data = json_decode($input_data, true); // validation
+// $input_data = file_get_contents("php://input");
+// $user_data = json_decode($input_data, true); // validation
 
-$userFirstname = $user_data["first_name"];
-$userLastname = $user_data["last_name"];
-$userEmail = $user_data["email"];
-$userPassword = $user_data["password"];
-$userStatus = $user_data["status"];
-$userGender = $user_data["gender"];
-$userCarNumber = $user_data["car_number"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userFirstname = $_POST["first_name"];
+    $userLastname = $_POST["last_name"];
+    $userEmail = $_POST["email"];
+    $userPassword = $_POST["password"];
+    $userStatus = $_POST["status"];
+    $userGender = $_POST["gender"];
+    $userCarNumber = $_POST["car_number"];
 
-// Hash password, Validation
-$user = new User($userFirstname, $userLastname, $userEmail, $userPassword, $userStatus, $userGender, $userCarNumber);
+    // Hash password, Validation
+    $user = new User($userFirstname, $userLastname, $userEmail, $userPassword, $userStatus, $userGender, $userCarNumber);
 
-$user_db = getUserByEmail($email); // validation
+    $user_db = getUserByEmail($email); // validation
 
-session_start();
-$_SESSION["user"] = $user_db;
+    session_start();
+    $_SESSION["user"] = $user_db;
 
-setcookie("email", $user["email"], time() + 1000, "/");
-setcookie("password", $user["password"], time() + 1000, "/");
+    http_response_code(200);
+    exit(json_encode(["status" => "SUCCESS", "message" => "Successfull registration"]));
+}
 
-http_response_code(200);
-exit(json_encode(["status" => "SUCCESS", "message" => "Successfull registration"]));
+// $userFirstname = $user_data["first_name"];
+// $userLastname = $user_data["last_name"];
+// $userEmail = $user_data["email"];
+// $userPassword = $user_data["password"];
+// $userStatus = $user_data["status"];
+// $userGender = $user_data["gender"];
+// $userCarNumber = $user_data["car_number"];
+
+// // Hash password, Validation
+// $user = new User($userFirstname, $userLastname, $userEmail, $userPassword, $userStatus, $userGender, $userCarNumber);
+
+// $user_db = getUserByEmail($email); // validation
+
+// session_start();
+// $_SESSION["user"] = $user_db;
+
+// setcookie("email", $user["email"], time() + 1000, "/");
+// setcookie("password", $user["password"], time() + 1000, "/");
+
+// http_response_code(200);
+// exit(json_encode(["status" => "SUCCESS", "message" => "Successfull registration"]));
 ?>
