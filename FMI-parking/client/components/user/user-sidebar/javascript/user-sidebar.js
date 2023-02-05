0be781ae729510d7a69d67b2a1a8bf3fe9e24428
Bozhidar Dimitrov <dbozhidar@vmware.com>
2023-02-05 13:06:36 +0200

    Display user name on top of sidebar

---
+++ FMI-parking/client/components/user/user-sidebar/javascript/user-sidebar.js
@@ -0,0 +1,26 @@
+var getNameFn = function getName() {
+  return getUserName().then((response) => {
+    return response["name"];
+  })
+  .catch((errorMessage) => {
+      console.log(errorMessage);
+  });
+};
+
+$(window).load(function() {
+  getNameFn().then(function(name) {
+    $("h2#user-title-id").html(name);
+  });
+});
+
+function getUserName() {
+    return fetch("../../../../server/controller/current_user.php", {
+        method: "GET",
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            return data;
+        })
+}
