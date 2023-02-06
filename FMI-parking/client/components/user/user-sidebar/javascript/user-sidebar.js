3bb126b023a87cae8bccf7346223ad731c52026c
Bozhidar Dimitrov <dbozhidar@vmware.com>
2023-02-06 19:21:04 +0200

    Front end typos and style

--- FMI-parking/client/components/user/user-sidebar/javascript/user-sidebar.js
+++ FMI-parking/client/components/user/user-sidebar/javascript/user-sidebar.js
@@ -1,3 +1,16 @@
+function deleteCookie() {
+    removeCookie("PHPSESSID");
+}
+
+function removeCookie(cookieName) {
+    cookieValue = "";
+    cookieLifetime = -1;
+    var date = new Date();
+    date.setTime(date.getTime()+(cookieLifetime*24*60*60*1000));
+    var expires = "; expires="+date.toGMTString();
+    document.cookie = cookieName+"="+JSON.stringify(cookieValue)+expires+"; path=/";
+}
+
 var getNameFn = function getName() {
   return getUserName().then((response) => {
     return response["name"];
